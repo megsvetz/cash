44dbbe6abbd0ab3efda2a13ffe8327eb13e45754
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-13 11:00:56 -0600

    newline in schema

--- db/schema.rb
+++ db/schema.rb
@@ -39,4 +39,5 @@ ActiveRecord::Schema.define(version: 20150613172222) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
+
 end

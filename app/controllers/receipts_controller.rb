7f054ca7225b1fbf9847186a45c358c60e196739
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-13 11:18:25 -0600

    changed name of groups controller to families, added actions for receipts controller

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,13 +1,51 @@
 class ReceiptsController < ApplicationController
+
   def index
+    @receipts = Receipt.all
   end
 
   def new
+    @receipt = Receipt.new
+  end
+
+  def create
+    @receipt = Receipt.find(params[:id])
+    if @receipt.update(receipt_params)
+      redirect_to receipts_index_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @receipt = Receipt.find(params[:id])
   end
 
-  def show
+  def update
+    @receipt = Receipt.find(params[:id])
+    if @receipt.update(receipt_params)
+      redirect_to receipts_index_path
+    else
+      render :edit
+    end
   end
+
+
+  def show
+    @receipt = Receipt.find(params[:id])
+  end
+
+  def destroy
+    @receipt = Receipt.find(params[:id])
+    @receipt.destroy
+    redirect_to receipts_index_path
+  end
+
+
+  private
+
+  def receipt_params
+    params.require(:receipt).permit(:name, :date, :amount, :description, :member_id)
+  end
+
 end

93af7dc0350c5b3b215cf24c99f7ec9e20e1783e
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-13 11:56:04 -0600

    added sessions controller, renamed groups controller to families

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    @family = Family.where(username: params[:username]).first
+    if @family.present? && @family.authenticate(params[:password])
+      session[:family_id] = @family.id
+      redirect_to root_path, notice: "Welcome to Cash Register!"
+    else
+      flash[:alert] = 'Wrong username or password'
+      render :new
+    end
+
+  def destroy
+    sessions[:family_id]
+    redirect_to members_index
   end
 end

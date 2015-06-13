c446c7fa4e649efe8a21e2891ddae99a8878c928
Meg Svetz <megsvetz@gmail.com>
2015-06-13 15:20:28 -0600

    changes to view files

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,38 +1,17 @@
 class FamiliesController < ApplicationController
 
-  def index
-    @families = Family.all
-  end
-
   def new
     @family = Family.new
   end
 
   def create
-    @family = Family.find(params[:id])
-    if @family.update(family_params)
-      redirect_to members_index_path
+    @family = Family.new(family_params)
+    raise
+    if @family.save
+      redirect_to login_path
     else
       render :new
     end
-  end
-
-  def edit
-    @family = Family.find(params[:id])
-  end
-
-  def update
-    @family = Family.find(params[:id])
-    if @family.update(family_params)
-      redirect_to sessions_new_path
-    else
-      render :edit
-    end
-  end
-
-  def show
-    @family = Family.find(params[:id])
-    @members = @family.members
   end
 
   def destroy
@@ -44,7 +23,7 @@ class FamiliesController < ApplicationController
   private
 
   def family_params
-    params.require(:family).permit(:username, :password)
+    params.require(:family).permit(:username, :password, :password_confirmation)
   end
 
 end
